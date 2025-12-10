2c3c229bc8f03cf7ca808afeb7ba53a518bc84d8
guilhermehcorrea8 <guilherme.h.correa8@aluno.senai.br>
2025-12-10 10:16:33 -0300

    informacoa

--- Program.cs
+++ Program.cs
@@ -2,36 +2,31 @@
 using aula_1012.obj;
 
 
-public class Program
+
+while (true)
 {
-    public static void Main()
+    Console.Clear();
+    Console.WriteLine("=== MENU ===");
+    Console.WriteLine("1 - Fazer Teste RIASEC");
+    Console.WriteLine("0 - Sair");
+    Console.Write("\nEscolha: ");
+
+    string opcao = Console.ReadLine()!;
+
+    if (opcao == "1")
     {
-        while (true)
-        {
-            Console.Clear();
-            Console.WriteLine("=== MENU ===");
-            Console.WriteLine("1 - Fazer Teste RIASEC");
-            Console.WriteLine("0 - Sair");
-            Console.Write("\nEscolha: ");
-
-            string opcao = Console.ReadLine()!;
-
-            if (opcao == "1")
-            {
-                Console.Clear();
-                var teste = new TesteRIASEC();
-                teste.Iniciar();
-            }
-            else if (opcao == "0")
-            {
-                Console.WriteLine("Saindo...");
-                return;
-            }
-            else
-            {
-                Console.WriteLine("Opção inválida!");
-                Console.ReadLine();
-            }
-        }
+        Console.Clear();
+        var teste = new TesteRIASEC();
+        teste.Iniciar();
+    }
+    else if (opcao == "0")
+    {
+        Console.WriteLine("Saindo...");
+        return;
+    }
+    else
+    {
+        Console.WriteLine("Opção inválida!");
+        Console.ReadLine();
     }
 }
